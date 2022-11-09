ca1bb4a7b31f6262067bcca71148cbfea113b8a9
Central-Cee <113004944+Central-Cee@users.noreply.github.com>
2022-11-09 10:27:49 +0530

    Add files via upload

---
+++ JavaMaven1/src/main/java/in/kmit/JavaMaven1/App.java
@@ -0,0 +1,13 @@
+package in.kmit.JavaMaven1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Aaaa aaa all we do is always number one" );
+    }
+}
